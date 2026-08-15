524f2524f249ce10443d98badb09fc2eca0875a3
y0bahrambeigi <miketherippa@gmail.com>
2026-08-15 16:40:33 -0700

    Refresh PWA cache for Persian audio fix

--- python-learning-studio-pro/sw.js
+++ python-learning-studio-pro/sw.js
@@ -1,5 +1,5 @@
-const CACHE="pls-ultra-v3";
-const CORE=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];
+const CACHE="pls-ultra-v4";
+const CORE=["./","./index.html","./styles.css","./app.js","./audio.js","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
